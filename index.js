ada2e7052ae3b5e992e74fb018ce77bbe84a4882
Jordy <jordynavalee@gmail.com>
2022-09-16 10:53:26 -0500

    express setup

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port),
+  () => {
+    console.log(`Express app listening on port: ${port}`);
+  };
